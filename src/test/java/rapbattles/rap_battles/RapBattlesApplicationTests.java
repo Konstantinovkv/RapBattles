3b2a18e7f50e93df5a1a9ad9b1a60e20396cde0a
Konstantin <konstantinovkv@gmail.com>
2019-04-17 17:58:15 +0300

    Initialization of the project + MySQL diagram.

---
+++ src/test/java/rapbattles/rap_battles/RapBattlesApplicationTests.java
@@ -0,0 +1,16 @@
+package rapbattles.rap_battles;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RapBattlesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
